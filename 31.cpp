ba9a9eb898379dc94c8668fc565c04d348ff8763
Sakshi Pokhriyal <sakshi.pokhriyal007@gmail.com>
2024-12-11 18:36:46 +0530

    Add files via upload

---
+++ 31.cpp
@@ -0,0 +1,18 @@
+//C++ program to demonstrate functioning of substr()
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    //Take any string 
+    string s1 = "Sakshi";
+
+    //Copy two characters of s1(starting 
+    // from index 3)
+    string r = s1.substr(3,2);
+
+    //prints the result
+    cout<<"String is:"<<r;
+
+    return 0;
+}
